7f34a0834ad384b539e529391697f7b70cbaa2d5
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-11 15:53:13 -0400

    add Avatar.js and Links.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './Footer.js'
 import Other from './Other.js'
 import Aside from './Aside.js'
 import Article from './Article.js'
+import Avatar from './Avatar.js'
+import Links from './Links.js'
 
 class App extends Component {
   render() {
@@ -14,24 +16,9 @@ class App extends Component {
         <main className="expanded row">
           <div className="large-8 medium-12 columns article">
             <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
-            <div className="avatar">
-              <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
-              <div className="author-info">
-                <p className="author-name">By Domri, son of Flug</p>
-                <p className="date">on 28 April, 3018 of the Third Age</p>
-              </div>
-            </div>
+            <Avatar />
             <Article />
-            <div className="article-links">
-              <a className="article-link" href="#">
-                <i className="fa fa-comments-o"></i>
-                <span className="article-link-text">Comments</span>
-              </a>
-              <a className="article-link" href="#">
-                <i className="fa fa-share"></i>
-                <span className="article-link-text">Share Post</span>
-              </a>
-            </div>
+            <Links />
           </div>
           <Aside />
           <Other />
