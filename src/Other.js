affbc6b7243cf5a1e0599e7907d7655c33184c41
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-11 19:11:45 -0400

    render article links using map

--- src/Other.js
+++ src/Other.js
@@ -4,15 +4,25 @@ import Other02 from './Other02.js'
 import Other03 from './Other03.js'
 import Other04 from './Other04.js'
 
+class Others extends React.Component {
+    render() {
+        return this.props.name
+    }
+}
+
 class Other extends Component {
+    constructor() {
+        super()
+        this.state = {
+            others: [<Other01 />, <Other02 />, <Other03 />, <Other04 />]
+        }
+    }
+
     render() {
         return (
             <div className="small-12 columns other-articles">
                 <h2>From around the Realm</h2>
-                <Other01 />
-                <Other02 />
-                <Other03 />
-                <Other04 />
+                {this.state.others.map(others => <Others name={others} />)}
             </div>
         )
     }
