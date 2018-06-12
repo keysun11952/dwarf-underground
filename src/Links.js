bf17298c110dd2cce2b2b10376b95bc1de52d5aa
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-11 22:15:03 -0400

    display comments that are submitted

--- src/Links.js
+++ src/Links.js
@@ -4,7 +4,10 @@ class Links extends Component {
     render() {
         return (
             <div className="article-links">
-                <a className="article-link" onClick={() => document.querySelector("textarea").removeAttribute("hidden")}>
+                <a className="article-link" onClick={() => {
+                    document.querySelector("textarea").removeAttribute("hidden")
+                    document.getElementById("submit").removeAttribute("hidden")
+                }}>
                     <i className="fa fa-comments-o"></i>
                     <span className="article-link-text">Comments</span>
                 </a>
@@ -12,7 +15,14 @@ class Links extends Component {
                     <i className="fa fa-share"></i>
                     <span className="article-link-text">Share Post</span>
                 </a>
-                <textarea name="comment" id="comment" cols="20" rows="2" hidden></textarea>
+                <textarea className="comment" cols="20" rows="2" hidden></textarea>
+                <a className="submit" id="submit" hidden onClick={() => {
+                    const comment = document.querySelector("textarea").value
+                    document.getElementById("comment").innerHTML += "Comment: " + comment + "<br />"
+                    document.getElementById("comment").removeAttribute("hidden")
+                    document.querySelector("textarea").value = ""
+                }}>submit</a>
+                <p id="comment" hidden></p>
             </div>
         )
     }
