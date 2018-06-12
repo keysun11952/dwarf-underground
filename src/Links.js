aa128edc06f210ad1b4e38802486ec693c3091e3
Junheng Wang <wangj14@rose-hulman.edu>
2018-06-11 20:09:04 -0400

    add text field that appear when link is clicked

--- src/Links.js
+++ src/Links.js
@@ -4,7 +4,7 @@ class Links extends Component {
     render() {
         return (
             <div className="article-links">
-                <a className="article-link" href="#">
+                <a className="article-link" onClick={() => document.querySelector("textarea").removeAttribute("hidden")}>
                     <i className="fa fa-comments-o"></i>
                     <span className="article-link-text">Comments</span>
                 </a>
@@ -12,6 +12,7 @@ class Links extends Component {
                     <i className="fa fa-share"></i>
                     <span className="article-link-text">Share Post</span>
                 </a>
+                <textarea name="comment" id="comment" cols="20" rows="2" hidden></textarea>
             </div>
         )
     }
